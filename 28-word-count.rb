2e83e9c23b9dc98e7be392280811b9d4f69770f7
Celery8911 <celery8911@gmail.com>
2017-10-03 18:38:44 +0800

    29,30

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,15 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
 doc = File.read("wordcount.txt")
+doc = doc.split("")
+h = {}
+doc.each do |word|
+  if h[word]
+    h[word] +=1
+  else
+    h[word] =1
+  end
+end
 
+puts h
 # ...
--- 29-todos.rb
+++ 29-todos.rb
@@ -11,18 +11,36 @@ todos.each_with_index do |todo, index|
   puts "#{index}: #{todo}"
 end
 
+def show(array)
+  array.each_with_index do |todo, index|
+    puts "#{index}: #{todo}"
+  end
+end
+
 while (true)
   print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
   command = gets.chomp
 
   if command == "add"
     print "请输入代办事项: "
+    newtodo = gets.chomp
+    todos.push(newtodo)
+    show(todos)
     # ...
   elsif command == "remove"
     print "请输入要删除的编号: "
     # ...
+    index = gets.chomp.to_i
+    todos.delete_at(index)
+    show(todos)
   elsif command == "save"
     puts "存盘离开"
+    File.open("todos.txt", "w+") do |f|
+      todos.each do |line|
+        f << line
+        f << "\n"
+      end
+    end
 
     # ...
     break;
@@ -30,4 +48,3 @@ while (true)
     puts "看不懂，请再输入一次"
   end
 end
-
